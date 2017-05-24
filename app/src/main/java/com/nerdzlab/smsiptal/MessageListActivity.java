347c44033e5a9aaff7f3f6905be91753e2cad8a0
orcunozyurt <orcunozyurt@hotmail.com>
2017-05-25 00:18:21 +0300

    cardview

--- app/src/main/java/com/nerdzlab/smsiptal/MessageListActivity.java
+++ app/src/main/java/com/nerdzlab/smsiptal/MessageListActivity.java
@@ -4,7 +4,6 @@ package com.nerdzlab.smsiptal;
 import android.content.Context;
 import android.content.Intent;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.support.annotation.NonNull;
@@ -13,41 +12,28 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Bundle;
-import android.provider.Telephony.Sms.Inbox;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 
 
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.ion.Ion;
+
 import com.nerdzlab.smsiptal.models.GroupedMessage;
 import com.nerdzlab.smsiptal.models.Message;
-import com.nerdzlab.smsiptal.utils.GetCursorTask;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 import static com.nerdzlab.smsiptal.models.Message.ITEMMAP;
 
 /**
